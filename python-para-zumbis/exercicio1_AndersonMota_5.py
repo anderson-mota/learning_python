3b23d1412edce998feecce924a5cb86e5c4af868
Anderson Mota <anderson.mota12@gmail.com>
2013-09-13 00:57:52 -0300

    Ajuste no Exercício 5 da lista 1, do curso Python para zumbis.

--- python-para-zumbis/exercicio1_AndersonMota_5.py
+++ python-para-zumbis/exercicio1_AndersonMota_5.py
@@ -1,14 +1,14 @@
 def calcularDesconto():
     try:
-        preco = input("Forneça o preço do produto:")
-        desconto = input("Percentual de desconto:")
+        preco = float(input("Forneça o preço do produto:"))
+        desconto = float(input("Percentual de desconto:"))
         valorDesconto = (preco / 100) * desconto
         preco = preco - valorDesconto
-    except:
-        print "Dados invalidos"
+    except ValueError:
+        print ("Dados informados são inválidos, devem conter apenas números.")
         calcularDesconto()
     else:
-        print "Valor descontato: %.2f" %valorDesconto
-        print "Preço com desconto: %.2f" %preco
+        print ("Valor descontato: %.2f" %valorDesconto)
+        print ("Preço com desconto: %.2f" %preco)
     
 calcularDesconto()
